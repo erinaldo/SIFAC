73423024f2e0513ae1ab813ce8d829f47721e08b
mruizoporta <mruiz.oporta@gmail.com>
2016-07-07 12:10:45 -0600

    inicio Proyecto

--- SIFAC/SIFAC/Form1.cs
+++ SIFAC/SIFAC/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Proyecto.Catalogos.Formularios;
+using Seguridad.Formularios;
 
 namespace SIFAC
 {
@@ -20,8 +21,9 @@ namespace SIFAC
 
         private void button1_Click(object sender, EventArgs e)
         {
-            frmStbMantCatalogo objCatalogos = new frmStbMantCatalogo();
-            objCatalogos.Show();
+
+            LoginForm objLogin = new LoginForm();
+            objLogin.Show();
         }
     }
 }
