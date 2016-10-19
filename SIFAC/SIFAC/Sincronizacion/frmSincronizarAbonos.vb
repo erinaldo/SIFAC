7021911e399332d84ec811286672eeab5377519c
escobarmaradiaga <escobarmaradiaga@gmail.com>
2016-10-19 13:25:37 -0600

    Sincronizar Abonos Completo
    Sincronizar Abonos Completo

--- SIFAC/SIFAC/Sincronizacion/frmSincronizarAbonos.vb
+++ SIFAC/SIFAC/Sincronizacion/frmSincronizarAbonos.vb
@@ -8,17 +8,20 @@ Imports Proyecto.Catalogos.Datos
 Public Class frmSincronizarAbonos
 
 #Region "Declaracion de Variables"
-    Public DtEmpleados, DtRuta, DtVentas, DtEstados, dtAplFacturasProformaDetalle As DataTable
+    Public DtEmpleados, DtRuta, DtAbonos, DtEstados, dtAplCobros As DataTable
     Dim objseg As SsgSeguridad
     Dim boolAprobar, boolConsultar, boolExportar, boolDesactivar As Boolean
-    Dim intEstadoFactProfRegistrado As Integer
+    Dim intEstadoAbonoRegistrado As Integer
+    Dim objReciboCaja As SccReciboCaja
+    Dim objReciboDetFactura As SccReciboDetFactura
+
 #End Region
 
 #Region "Procedimientos"
 
     Private Sub InicializarVariables()
         Try
-            intEstadoFactProfRegistrado = CInt(ClsCatalogos.GetValorCatalogoID("ESTADOAPLICACION", "01"))
+            intEstadoAbonoRegistrado = CInt(ClsCatalogos.GetValorCatalogoID("ESTADOAPLICACION", "01"))
         Catch ex As Exception
             clsError.CaptarError(ex)
         End Try
@@ -65,7 +68,6 @@ Public Class frmSincronizarAbonos
         End Try
     End Sub
 
-
     Private Sub CargarEstados()
         DtEstados = ClsCatalogos.GetValoresCatalogo("ESTADOAPLICACION", "StbValorCatalogoID,Descripcion", "")
         DtEstados.DefaultView.Sort = "Descripcion"
@@ -89,26 +91,26 @@ Public Class frmSincronizarAbonos
         Try
 
             If blnTodos Then
-                DtVentas = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "1=1"))
+                DtAbonos = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "1=1"))
             End If
 
             If Not blnTodos And (intEmpleadoID <> 0 Or IsDBNull(intEmpleadoID)) And (intRutaID <> 0 Or IsDBNull(intRutaID)) Then
-                DtVentas = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "ObjRutaID =" & intRutaID & " AND SrhEmpleadoID=" & intEmpleadoID & " AND objEstadoID=" & intEstadoID))
+                DtAbonos = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "ObjRutaID =" & intRutaID & " AND SrhEmpleadoID=" & intEmpleadoID & " AND objEstadoID=" & intEstadoID))
             End If
 
             If Not blnTodos And (intEmpleadoID <> 0 Or IsDBNull(intEmpleadoID)) And Not (intRutaID <> 0 Or IsDBNull(intRutaID)) Then
-                DtVentas = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", " SrhEmpleadoID=" & intEmpleadoID & " AND objEstadoID=" & intEstadoID))
+                DtAbonos = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", " SrhEmpleadoID=" & intEmpleadoID & " AND objEstadoID=" & intEstadoID))
             End If
 
             If Not blnTodos And Not (intEmpleadoID <> 0 Or IsDBNull(intEmpleadoID)) And (intRutaID <> 0 Or IsDBNull(intRutaID)) Then
-                DtVentas = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "ObjRutaID =" & intRutaID & "  AND objEstadoID=" & intEstadoID))
+                DtAbonos = DAL.SqlHelper.ExecuteQueryDT(ObtenerConsultaGeneral("*", "vwAplicacionAbonos", "ObjRutaID =" & intRutaID & "  AND objEstadoID=" & intEstadoID))
             End If
 
-            If Not DtVentas Is Nothing Then
-                DtVentas.PrimaryKey = New DataColumn() {Me.DtVentas.Columns("AplCobroID")}
-                DtVentas.DefaultView.Sort = "FechaAbono"
-                Me.grdVentas.DataSource = DtVentas
-                Me.grdVentas.Text = "Abonos (" & Me.DtVentas.Rows.Count & ")"
+            If Not DtAbonos Is Nothing Then
+                DtAbonos.PrimaryKey = New DataColumn() {Me.DtAbonos.Columns("AplCobroID")}
+                DtAbonos.DefaultView.Sort = "FechaAbono"
+                Me.grdVentas.DataSource = DtAbonos
+                Me.grdVentas.Text = "Abonos (" & Me.DtAbonos.Rows.Count & ")"
             End If
 
         Catch ex As Exception
@@ -178,11 +180,11 @@ Public Class frmSincronizarAbonos
         Dim blnSeleccionar As Boolean
         Dim chkSeleccionado As DevExpress.XtraEditors.CheckEdit
 
-        intEstadoFila = CInt(DtVentas.Rows(FilaActual)("objEstadoID"))
+        intEstadoFila = CInt(DtAbonos.Rows(FilaActual)("objEstadoID"))
 
-        If intEstadoFila = intEstadoFactProfRegistrado Then
+        If intEstadoFila = intEstadoAbonoRegistrado Then
             blnSeleccionar = CType(sender, DevExpress.XtraEditors.CheckEdit).Checked
-            DtVentas = objConsultas.actualizarColumnaDT(DtVentas, "SfaFacturaProformaID", CInt(DtVentas.Rows(FilaActual)("SfaFacturaProformaID")), "Seleccionar", blnSeleccionar)
+            DtAbonos = objConsultas.actualizarColumnaDT(DtAbonos, "AplCobroID", CInt(DtAbonos.Rows(FilaActual)("AplCobroID")), "Seleccionar", blnSeleccionar)
         Else
             chkSeleccionado = CType(sender, DevExpress.XtraEditors.CheckEdit)
             chkSeleccionado.Checked = False
@@ -190,174 +192,87 @@ Public Class frmSincronizarAbonos
 
     End Sub
 
-    Private Sub SincronizarVentas()
-        Dim objSfaFactura As SfaFacturas
-        Dim SfaFacturaID As Integer
-        Dim dtFechaVencimiento As Date
-        Dim SfaFacturaProformaID, objModalidadPagoID, objTerminoPagoID, intEstadoRegistrado As Integer
-
-        Dim objSfaFacturaDetalle As SfaFacturasDetalle
-        Dim objAplFacturasProforma As New AplFacturasProforma
-        Dim objAplFacturasProformaDetalle As New AplFacturasProformaDetalle
-        Dim dtDetalleFact, dtDetalleCuentasXCobrar As New DataTable
-        Dim fila As DataRow
-
-        Dim objSccCuenta As SccCuentaPorCobrar
-        Dim objSccCuentaPorCobrarDetalle As SccCuentaPorCobrarDetalle
-
-        Dim objStbValorCatalogo As StbValorCatalogo
-        Dim intFactorTerminoPago, intFactorModalidadPago As Integer
-        Dim blnSeleccionado As Boolean = False
+    Private Sub SincronizarAbonos()
+        Dim DtDatosFactRecibo As DataTable
+        DtDatosFactRecibo = New DataTable
+        Dim sConsulta As String
+        Dim intEstadoRegistrado As Integer
+        Dim blnSeleccionado As Boolean
+        Dim intAplCobroID, intobjSccCuentaID, intReciboCajaID As Integer
+        Dim dtFechaAbono As Date
+        Dim decTotalRecibo As Decimal
+        Dim objAplCobros As New AplCobros
 
         Dim t As New TransactionManager
 
         Try
             t.BeginTran()
 
-            'carga de estructura detalle cuentas por cobrar y detalle de la factura
-            dtDetalleCuentasXCobrar = SccCuentaPorCobrarDetalle.RetrieveDT("1=0", , , t)
-            dtDetalleFact = SfaFacturasDetalle.RetrieveDT("1=0", , , t)
+            For Each drFilaAbonos As DataRow In DtAbonos.Rows
+                blnSeleccionado = CBool(drFilaAbonos("Seleccionar"))
+                intEstadoRegistrado = CInt(drFilaAbonos("objEstadoID"))
 
-            objSfaFactura = New SfaFacturas
-            objAplFacturasProforma = New AplFacturasProforma
-
-            For Each drFilaVenta As DataRow In DtVentas.Rows
-
-                blnSeleccionado = CBool(drFilaVenta("Seleccionar"))
-                intEstadoRegistrado = CInt(drFilaVenta("objEstadoID"))
-
-                If blnSeleccionado And intEstadoRegistrado = intEstadoFactProfRegistrado Then
+                If blnSeleccionado And intEstadoRegistrado = intEstadoAbonoRegistrado Then
                     'Asignacion de valores a Variables
-                    SfaFacturaProformaID = CInt(drFilaVenta("SfaFacturaProformaID"))
-                    dtFechaVencimiento = CDate(drFilaVenta("FechaVencimiento"))
-                    objModalidadPagoID = CInt(drFilaVenta("objModalidadPagoID"))
-                    objTerminoPagoID = CInt(drFilaVenta("objTerminoPagoID"))
+                    intAplCobroID = CInt(drFilaAbonos("AplCobroID"))
+                    intobjSccCuentaID = CInt(drFilaAbonos("objSccCuentaID"))
+                    dtFechaAbono = CDate(drFilaAbonos("FechaAbono"))
+                    decTotalRecibo = CDec(drFilaAbonos("MontoAbonado"))
 
                     '0 Actualizar estado factura proforma
-                    With objAplFacturasProforma
-                        .Retrieve(SfaFacturaProformaID, t)
+                    With objAplCobros
+                        .Retrieve(intAplCobroID, t)
                         .objEstadoID = CInt(ClsCatalogos.GetValorCatalogoID("ESTADOAPLICACION", "02"))
                         .Update(t)
                     End With
 
-                    '1. GUARDAR CABECERA DE FACTURA
-                    With objSfaFactura
-                        .Fecha = CDate(drFilaVenta("Fecha"))
-                        .objSccClienteID = CInt(drFilaVenta("objSccClienteID"))
-                        .objVendedorID = CInt(drFilaVenta("objVendedorID"))
-                        .objEstadoID = CInt(ClsCatalogos.GetValorCatalogoID("ESTADOFACT", "PROCESADA"))
-                        .objTerminoPagoID = CInt(ClsCatalogos.GetValorCatalogoID("TERMINOPAGO", "02"))
-                        .objDescuentoID = CInt(drFilaVenta("objDescuentoID"))
-                        .Subtotal = CDec(drFilaVenta("Subtotal"))
-                        .Descuento = CDec(drFilaVenta("Descuento"))
-                        .Impuesto = 0
-                        .TotalCordobas = CDec(drFilaVenta("Total"))
-                        .TotalDolares = 0
-                        .TasaCambio = 0
-                        .Prima = CDec(drFilaVenta("Prima"))
-                        .Saldo = CDec(drFilaVenta("Saldo"))
-                        .objStbBodegaID = ClsCatalogos.GetStbTiendaID(clsProyecto.Sucursal)
-                        .UsuarioCreacion = clsProyecto.Conexion.Usuario
-                        .FechaCreacion = clsProyecto.Conexion.FechaServidor
-                        .Insert(t)
-                        SfaFacturaID = .SfaFacturaID
-                    End With
+                    '--------------------------- Creamos el encabezado del recibo colector--------------------------
+                    Me.objReciboCaja = New SccReciboCaja
+                    Me.objReciboCaja.Numero = 0
+                    Me.objReciboCaja.Fecha = dtFechaAbono
+                    Me.objReciboCaja.TotalRecibo = decTotalRecibo
+                    Me.objReciboCaja.UsuarioCreacion = clsProyecto.Conexion.Usuario
+                    Me.objReciboCaja.FechaCreacion = clsProyecto.Conexion.FechaServidor
+                    Me.objReciboCaja.objEstadoID = ClsCatalogos.ObtenerIDSTbCatalogo("EstadoROC", "PAGADO")
+                    Me.objReciboCaja.objSccCuentaID = intobjSccCuentaID
+                    Me.objReciboCaja.EsPagoPrima = False
+                    '-------------------------------------------------------------------- ---------------------------
+                    Me.objReciboCaja.Insert(t)
+                    intReciboCajaID = Me.objReciboCaja.SccReciboCajaID
 
-                    '2. GUARDAR DETALLE DE FACTURA
+                    'Cargar Facturas por cuentas
+                    sConsulta = clsConsultas.ObtenerConsultaGeneral("*,Saldo as SaldoFactura,CAST(0 AS BIT) AS Seleccion,CAST(0 AS BIT) AS Abonar,CAST(0 AS BIT) AS Cancelar,CAST(0 AS DECIMAL) as CantAbonar", "vwSccReciboDetFactura", "objSccCuentaID='" & intobjSccCuentaID & "'")
+                    DtDatosFactRecibo = SqlHelper.ExecuteQueryDT(sConsulta)
 
-                    objSfaFacturaDetalle = New SfaFacturasDetalle
-                    objAplFacturasProformaDetalle = New AplFacturasProformaDetalle
-
-                    '2.1. Filtrar el detalle de Factura Proforma
-                    dtAplFacturasProformaDetalle = objAplFacturasProformaDetalle.RetrieveDT(String.Format("objSfaFacturaProformaID={0}", SfaFacturaProformaID), , , t)
-
-                    For Each row As DataRow In dtAplFacturasProformaDetalle.Rows
-                        fila = dtDetalleFact.NewRow
-                        fila("objSfaFacturaID") = SfaFacturaID
-                        fila("objSivProductoID") = row("objSivProductoID")
-                        fila("Cantidad") = row("Cantidad")
-                        fila("Precio") = row("Precio")
-                        fila("Subtotal") = row("Subtotal")
-                        fila("Descuento") = row("Descuento")
-                        fila("Impuesto") = 0 'row("Impuesto")
-                        fila("Total") = row("Total")
-                        fila("CostoDolares") = 0 'row("CostoPromedio")
-                        fila("UsuarioCreacion") = clsProyecto.Conexion.Usuario
-                        fila("FechaCreacion") = clsProyecto.Conexion.FechaServidor
-                        dtDetalleFact.Rows.Add(fila)
+                    '---------------------------------------------------------------------------------------------
+                    '-----------------Creamos los detalles por cada factura procesada-----------------------------
+                    objReciboDetFactura = New SccReciboDetFactura
+                    For Each drwFactINC As DataRow In DtDatosFactRecibo.Rows
+                        objReciboDetFactura.Monto = (Me.objReciboCaja.TotalRecibo / DtDatosFactRecibo.Rows.Count)
+                        objReciboDetFactura.objSfaFacturaID = drwFactINC("SfaFacturaID")
+                        objReciboDetFactura.UsuarioCreacion = clsProyecto.Conexion.Usuario
+                        objReciboDetFactura.FechaCreacion = clsProyecto.Conexion.FechaServidor
+                        objReciboDetFactura.objReciboCajaID = Me.objReciboCaja.SccReciboCajaID
+                        objReciboDetFactura.EsAbono = True
+                        objReciboDetFactura.Insert(t)
                     Next
 
-                    dtDetalleFact.TableName = "SfaFacturasDetalle"
-                    SfaFacturasDetalle.BatchUpdate(dtDetalleFact.DataSet, t)
+                    'Procesamos el Abono
+                    Dim parametro(3) As SqlClient.SqlParameter
+                    parametro(0) = New SqlClient.SqlParameter("@IntReciboCajaID", SqlDbType.Int, 4)
+                    parametro(0).Value = Me.objReciboCaja.SccReciboCajaID
 
-                    '3. VERIFICAR SI EL CLIENTE TIENE CUENTA VIGENTE
-                    objSccCuenta = New SccCuentaPorCobrar
+                    parametro(1) = New SqlClient.SqlParameter("@EsPrima", SqlDbType.Int, 4)
+                    parametro(2) = New SqlClient.SqlParameter("@MontoPrima", SqlDbType.Decimal)
+                    parametro(1).Value = 0
+                    parametro(2).Value = 0
 
-                    Dim blnCuentaActiva As Boolean
-                    blnCuentaActiva = objSccCuenta.RetrieveByFilter(String.Format("objClienteID={0} AND objEstadoID={1}", objSfaFactura.objSccClienteID, CInt(ClsCatalogos.GetValorCatalogoID("ESTADOEXPEDIENTE", "VIGENTE"))), t)
-
-                    'Buscar Factor termino de pago y modalidad de pago
-                    objStbValorCatalogo = New StbValorCatalogo
-
-                    objStbValorCatalogo.Retrieve(objTerminoPagoID, t)
-                    intFactorTerminoPago = CInt(objStbValorCatalogo.Codigo)
-
-                    objStbValorCatalogo.Retrieve(objModalidadPagoID, t)
-                    intFactorModalidadPago = CInt(objStbValorCatalogo.Codigo)
-
-                    If blnCuentaActiva Then
-
-                        'Actualizar Estado y saldo de la cuenta por cobrar
-                        objSccCuenta.Saldo = objSccCuenta.Saldo + objSfaFactura.Saldo
-                        objSccCuenta.UsuarioModificacion = clsProyecto.Conexion.Usuario
-                        objSccCuenta.FechaModificacion = clsProyecto.Conexion.FechaServidor
-                        objSccCuenta.Update(t)
-
-                        '3.1 GUARDAR DETALLE CUENTA
-                        GoTo lblGuardarDetalleCuenta
-
-
-                    Else
-                        '3.2 GUARDAR CUENTA
-                        With objSccCuenta
-                            .Saldo = objSfaFactura.Saldo
-                            .UsuarioCreacion = clsProyecto.Conexion.Usuario
-                            .SaldoInicial = 0.0
-                            .FechaCreacion = clsProyecto.Conexion.FechaServidor
-                            .objClienteID = objSfaFactura.objSccClienteID
-                            .FechaCredito = objSfaFactura.Fecha
-                            .objEstadoID = ClsCatalogos.ObtenerIDSTbCatalogo("ESTADOEXPEDIENTE", "VIGENTE")
-                            objSccCuenta.Insert(t)
-                        End With
-
-lblGuardarDetalleCuenta:
-
-                        '3.2 GUARDAR DETALLE CUENTA
-                        objSccCuentaPorCobrarDetalle = New SccCuentaPorCobrarDetalle
-
-                        With objSccCuentaPorCobrarDetalle
-                            .objSccCuentaID = objSccCuenta.SccCuentaID
-                            .objSfaFacturaID = SfaFacturaID
-                            .objTeminoPlazoID = objTerminoPagoID
-                            .objEstadoID = ClsCatalogos.ObtenerIDSTbCatalogo("ESTADOCUENTA", "01")
-                            .Saldo = objSfaFactura.Saldo
-                            .MontoTotal = objSfaFactura.Subtotal
-                            .MontoPrima = objSfaFactura.Prima
-                            .MontoAbonado = 0
-                            .MontoCuota = (objSfaFactura.Saldo / intFactorTerminoPago)
-                            .Descuento = objSfaFactura.Descuento
-                            .FechaProximoPago = DateTime.Parse(objSfaFactura.Fecha).AddDays(intFactorModalidadPago)
-                            .FechaVencimiento = dtFechaVencimiento
-                            .UsuarioCreacion = clsProyecto.Conexion.Usuario
-                            .FechaCreacion = clsProyecto.Conexion.FechaServidor
-                            .Insert(t)
-                        End With
-                    End If
-
-                    MsgBox(My.Resources.MsgAgregado, MsgBoxStyle.Information + MsgBoxStyle.OkOnly, clsProyecto.SiglasSistema)
+                    SqlHelper.ExecuteNonQuery(t.Transaction, CommandType.StoredProcedure, "Sp_SfaDimininuirCuentaPorCobrar", parametro)
 
                 End If
+
             Next
+
 
             t.CommitTran()
         Catch ex As Exception
@@ -366,7 +281,7 @@ lblGuardarDetalleCuenta:
         End Try
     End Sub
 
-    Private Sub AnularVentas()
+    Private Sub AnularAbonos()
         Dim SfaFacturaProformaID, intEstadoRegistrado As Integer
         Dim objAplFacturasProforma As New AplFacturasProforma
         Dim blnSeleccionado As Boolean = False
@@ -378,12 +293,12 @@ lblGuardarDetalleCuenta:
 
             objAplFacturasProforma = New AplFacturasProforma
 
-            For Each drFilaVenta As DataRow In DtVentas.Rows
+            For Each drFilaVenta As DataRow In DtAbonos.Rows
 
                 blnSeleccionado = CBool(drFilaVenta("Seleccionar"))
                 intEstadoRegistrado = CInt(drFilaVenta("objEstadoID"))
 
-                If blnSeleccionado And intEstadoRegistrado = intEstadoFactProfRegistrado Then
+                If blnSeleccionado And intEstadoRegistrado = intEstadoAbonoRegistrado Then
                     With objAplFacturasProforma
                         .Retrieve(SfaFacturaProformaID, t)
                         .objEstadoID = CInt(ClsCatalogos.GetValorCatalogoID("ESTADOAPLICACION", "03"))
@@ -401,7 +316,7 @@ lblGuardarDetalleCuenta:
     End Sub
 
     Private Sub cmdAprobar_Click(sender As Object, e As EventArgs) Handles cmdAprobar.Click
-        SincronizarVentas()
+        SincronizarAbonos()
         CargarGrid(chkTodos.Checked, cmbEstado.EditValue, cmbEmpleado.EditValue, cmbRuta.EditValue)
     End Sub
 
@@ -414,6 +329,6 @@ lblGuardarDetalleCuenta:
     End Sub
 
     Private Sub cmdDesactivar_Click(sender As Object, e As EventArgs) Handles cmdDesactivar.Click
-        AnularVentas()
+        AnularAbonos()
     End Sub
 End Class
